657b685040fe5e7f20d1455dc6b53c83c3608406
Damian Wicik <damian.wicik20@gmail.com>
2025-05-31 10:26:23 +0200

    map list

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   ComposableMap,
   Geographies,
@@ -7,9 +7,49 @@ import {
 } from 'react-simple-maps'
 import { Tooltip } from 'react-tooltip'
 import GeoPoland from '@/lib/geo-poland.json'
+import servicesList from '@/assets/data/services.json'
+
+// Example voivodeship data with coordinates and a number
+const voivodeshipsInit = [
+  { name: 'Dolnośląskie', coordinates: [16.33, 51.1079], number: 1 },
+  { name: 'Kujawsko-Pomorskie', coordinates: [18.6, 53.0138], number: 2 },
+  { name: 'Lubelskie', coordinates: [22.5667, 51.25], number: 3 },
+  { name: 'Lubuskie', coordinates: [15.2, 52.2333], number: 4 },
+  { name: 'Łódzkie', coordinates: [19.4667, 51.7667], number: 5 },
+  { name: 'Małopolskie', coordinates: [20.0199, 49.8951], number: 6 },
+  { name: 'Mazowieckie', coordinates: [21.0122, 52.2297], number: 7 },
+  { name: 'Opolskie', coordinates: [17.9333, 50.6667], number: 8 },
+  { name: 'Podkarpackie', coordinates: [22.0, 50.0167], number: 9 },
+  { name: 'Podlaskie', coordinates: [23.1667, 53.1333], number: 10 },
+  { name: 'Pomorskie', coordinates: [18.1383, 54.152], number: 11 },
+  { name: 'Śląskie', coordinates: [18.93, 50.2945], number: 12 },
+  { name: 'Świętokrzyskie', coordinates: [20.6333, 50.8667], number: 13 },
+  { name: 'Warmińsko-Mazurskie', coordinates: [20.5, 53.7667], number: 14 },
+  { name: 'Wielkopolskie', coordinates: [16.9252, 52.4064], number: 15 },
+  { name: 'Zachodniopomorskie', coordinates: [15.553, 53.5285], number: 16 },
+]
 
 export const Map = () => {
   const [tooltipContent, setTooltipContent] = useState('')
+  const [voivodeships, setVoivodeships] = useState(voivodeshipsInit)
+
+  useEffect(() => {
+    setVoivodeships((prevVoivodeships) => {
+      const counts = servicesList.reduce(
+        (acc, product) => {
+          product.attributes.wojewodztwa.forEach((v) => {
+            acc[v] = (acc[v] || 0) + 1
+          })
+          return acc
+        },
+        {} as Record<string, number>,
+      )
+      return prevVoivodeships.map((voivodeship) => ({
+        ...voivodeship,
+        number: counts[voivodeship.name.toLocaleLowerCase()] || 0,
+      }))
+    })
+  }, [])
 
   return (
     <>
@@ -17,8 +57,8 @@ export const Map = () => {
         projection="geoMercator"
         style={{ userSelect: 'none' }}
         projectionConfig={{
-          center: [19.1451, 52.237], // approximate center of Poland (longitude, latitude)
-          scale: 3500, // Adjust this number to zoom in/out
+          center: [19.1451, 52.237],
+          scale: 3500,
         }}
         width={800}
         height={600}
@@ -29,7 +69,7 @@ export const Map = () => {
               <Geography
                 key={geo.rsmKey}
                 geography={geo}
-                data-tooltip-id="tooltip" // Link to the Tooltip
+                data-tooltip-id="tooltip"
                 onMouseEnter={() => {
                   const { name } = geo.properties
                   setTooltipContent(`${name}`)
@@ -53,14 +93,27 @@ export const Map = () => {
             ))
           }
         </Geographies>
-        <Marker coordinates={[21.0122, 52.2297]}>
-          <circle r={5} fill="#F00" />
-        </Marker>
+
+        {voivodeships.map((voivode, index) => (
+          <Marker
+            key={index}
+            coordinates={voivode.coordinates as [number, number]}
+          >
+            <circle r={16} fill="#f44336" stroke="#fff" strokeWidth={1.5} />
+            <text
+              textAnchor="middle"
+              y={5}
+              fontSize={14}
+              fill="#fff"
+              fontWeight="bold"
+            >
+              {voivode.number}
+            </text>
+          </Marker>
+        ))}
       </ComposableMap>
-      <Tooltip
-        id="tooltip" // Tooltip ID that links with Geography via data-tooltip-id
-        place="top" // You can use 'top', 'bottom', 'left', or 'right'
-      >
+
+      <Tooltip id="tooltip" place="top">
         {tooltipContent}
       </Tooltip>
     </>
